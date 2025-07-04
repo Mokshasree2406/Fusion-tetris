a0a357e9796524410ce6b82b3e13d3f3f48ce63b
Mokshasree2406 <durgammokshasree@gmail.com>
2025-07-04 20:02:54 +0530

    updated Block class

--- src/Block.java
+++ src/Block.java
@@ -1,2 +1,42 @@
-public class Block {
+import javax.swing.text.Position;
+
+public abstract class Block {
+    protected Position position;
+
+    public Block(Position position){
+        this.position = position;
+    }
+
+    public Position  getPosition(){
+        return position;
+    }
+    public void setPosition(Position position){
+        this.position = position;
+    }
+
+    public abstract void interact(GameBoard board);
+
+    public boolean isEmpty(){
+        return this instanceof EmptyBlock;
+    }
+
+    public String getType(){
+        return this.getClass().getSimpleName();
+    }
+}
+
+public class NoPowerBlock extends Block{
+    public NoPowerBlock(Position position){
+        super(position);
+    }
+    public void interact(GameBoard board){
+        //we usually do nothing if the block is of nopwr , no function
+    }
+}
+
+
+public class FireBlock extends Block{
+    public FireBlock(Position position){
+        super(position);
+    }
 }
