8317eca9f28b759aad20e6765b333c75d4b27dbe
Mokshasree2406 <durgammokshasree@gmail.com>
2025-07-05 09:54:30 +0530

    Updated EarthBlock and AirBlock

--- src/AirBlock.java
+++ src/AirBlock.java
@@ -4,8 +4,39 @@ public class AirBlock extends Block {
     }
 
     public boolean interact(GameBoard board) {
+        int row = position.getRow();
+        int col = position.getCol();
 
-        board.shuffleTopLayer();
+        int rowCount = 1;
+        int colCount = 1;
+
+        for (int i = col - 1; i >= 0; i--) {
+            Block b = board.getBlockAt(row, i);
+            if (b instanceof AirBlock) rowCount++;
+            else break;
+        }
+
+        for (int i = col + 1; i < 10; i++) {
+            Block b = board.getBlockAt(row, i);
+            if (b instanceof AirBlock) rowCount++;
+            else break;
+        }
+
+        for (int i = row - 1; i >= 0; i--) {
+            Block b = board.getBlockAt(i, col);
+            if (b instanceof AirBlock) colCount++;
+            else break;
+        }
+
+        for (int i = row + 1; i < 10; i++) {
+            Block b = board.getBlockAt(i, col);
+            if (b instanceof AirBlock) colCount++;
+            else break;
+        }
+        if(rowCount>=3 || colCount >= 3) {
+            board.shuffleTopLayer();
+            return true;
+        }
         return false;
     }
 }
--- src/EarthBlock.java
+++ src/EarthBlock.java
@@ -4,7 +4,40 @@ public class EarthBlock extends Block {
     }
 
     public boolean interact(GameBoard board) {
-        board.shuffleRow(9);
+        int row = position.getRow();
+        int col = position.getCol();
+
+        int rowCount = 1;
+        int colCount = 1;
+
+        for (int i = col - 1; i >= 0; i--) {
+            Block b = board.getBlockAt(row, i);
+            if (b instanceof EarthBlock) rowCount++;
+            else break;
+        }
+
+        for (int i = col + 1; i < 10; i++) {
+            Block b = board.getBlockAt(row, i);
+            if (b instanceof EarthBlock) rowCount++;
+            else break;
+        }
+
+        for (int i = row - 1; i >= 0; i--) {
+            Block b = board.getBlockAt(i, col);
+            if (b instanceof EarthBlock) colCount++;
+            else break;
+        }
+
+        for (int i = row + 1; i < 10; i++) {
+            Block b = board.getBlockAt(i, col);
+            if (b instanceof EarthBlock) colCount++;
+            else break;
+        }
+        if(rowCount>=3 || colCount >= 3){
+            board.shuffleRow(9);
+            return true;
+        }
+
         return false;
     }
 }
